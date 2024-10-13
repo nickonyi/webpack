9e84e3c55f1d77235e7b7fe8e84252cb8a592158
phatommobb <nickoluoch1130@gmail.com>
2024-10-13 16:32:32 +0300

    webpack code

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/index.html"],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
@@ -21,6 +25,16 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      }
+      
+      
     ],
   },
 };
